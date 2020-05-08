dd2df82c34943df3724a87fe356de59b22adfb40
jabialowas <bialowas.ja@gmail.com>
2020-05-08 12:34:51 +0200

    Updates

---
+++ precache-manifest.fd678a8824ebb26d9e249a8fb0d27533.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dcb6a15280886c39afc9c75ee8d7922",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e04c77cd5ce0a7101fe",
+    "url": "/static/css/2.c053574b.chunk.css"
+  },
+  {
+    "revision": "db7cbec5b3c24e233b81",
+    "url": "/static/css/main.d8b7b31a.chunk.css"
+  },
+  {
+    "revision": "3e04c77cd5ce0a7101fe",
+    "url": "/static/js/2.2c194b16.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/static/js/2.2c194b16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db7cbec5b3c24e233b81",
+    "url": "/static/js/main.d576e2ea.chunk.js"
+  },
+  {
+    "revision": "069221cd02efc8aeaa1e",
+    "url": "/static/js/runtime-main.025019d3.js"
+  }
+]);
