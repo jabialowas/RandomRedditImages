c7d9557ef63349056dd4277a97a3ac1bc15662af
jabialowas <bialowas.ja@gmail.com>
2020-05-08 12:39:18 +0200

    Updates

---
+++ precache-manifest.1597945162ae3128ff9edc0241877db5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe693cb91457879cf5172d92c650cafd",
+    "url": "/RedditGibImages/index.html"
+  },
+  {
+    "revision": "3e04c77cd5ce0a7101fe",
+    "url": "/RedditGibImages/static/css/2.c053574b.chunk.css"
+  },
+  {
+    "revision": "ca8861767d631d1ad442",
+    "url": "/RedditGibImages/static/css/main.d8b7b31a.chunk.css"
+  },
+  {
+    "revision": "3e04c77cd5ce0a7101fe",
+    "url": "/RedditGibImages/static/js/2.2c194b16.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/RedditGibImages/static/js/2.2c194b16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca8861767d631d1ad442",
+    "url": "/RedditGibImages/static/js/main.88a01b73.chunk.js"
+  },
+  {
+    "revision": "b5edf3bbc470d58086ee",
+    "url": "/RedditGibImages/static/js/runtime-main.84c12a5d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fd678a8824ebb26d9e249a8fb0d27533.js"
+  "/RedditGibImages/precache-manifest.1597945162ae3128ff9edc0241877db5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/RedditGibImages/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
